ed86fe4bf5780d79cebebae5c76585616ab4c71f
Giovanni Andrade Nespoli <22159641@educ123.sp.senai.br>
2022-08-08 19:33:06 -0300

    Python

---
+++ Python/aula01/commits.py
@@ -0,0 +1,11 @@
+# frase = input("Digite uma frase: "); -> input é uma forma de conseguir dados, 
+# igual a classe Scanner em Java e no ConsoleReadLine no c#
+
+# conjunto = (1,2,3); -> array
+
+# print(1+1) -> print é a forma de mostrar a informação, como sout no java e cwl no c#
+
+# Alguns exemplos de print : 
+# print(1**2) -> 1 elevado a 2
+# print(5%2) -> resto da divisao de 5 por 2
+# print(1 in conjunto) -> retorna um boolean
---
+++ Python/aula01/exs/ex01.py
@@ -0,0 +1,8 @@
+# conversor de metros para cm / mm
+
+num = int(input('Digíte um número em METROS: '));
+
+cm = num * 100;
+mm = num * 1000;
+
+print(str(num)+"m são "+str(cm)+"cm e "+str(mm)+"mm");
