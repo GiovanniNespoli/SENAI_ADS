209005ed4fac8f9c154c70eb5aa65444ed2c5d15
Giovanni Andrade Nespoli <22159641@educ123.sp.senai.br>
2022-08-08 21:07:23 -0300

    added ex 04

---
+++ Python/aula01/exs/ex03.py
@@ -0,0 +1,4 @@
+ganhoHora = int(input('R$ por hora: '));
+horaMes = int(input('Horas por mês: '));
+
+print(f'Você ganha por mês: {ganhoHora*horaMes}');
---
+++ Python/aula01/exs/ex04.py
@@ -0,0 +1,14 @@
+# coversão de fahrenheit de Cº
+
+fahrenheit = int(input('Temperatura em fahrenheit: '));
+
+convesaoCelsius = (fahrenheit - 32) * 5/9;
+
+print(fahrenheit,'Fahreinheit convertido para celsius:', convesaoCelsius);
+
+celsius = int(input('Temperatura em celsius: '));
+
+conversaoFahrenheit = (celsius * 9/5) + 32;
+
+print(celsius,'Celsius convertido para Fahreinheit', conversaoFahrenheit);
+
